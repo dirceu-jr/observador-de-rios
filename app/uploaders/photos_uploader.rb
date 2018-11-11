7c4ca9e159024129836396739c6f43aa9e6d2b25
dirceupj <dirceuu@gmail.com>
2018-11-11 17:13:59 -0200

    use imagemagick instead of libvips

--- app/uploaders/photos_uploader.rb
+++ app/uploaders/photos_uploader.rb
@@ -1,8 +1,8 @@
 class PhotosUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  include CarrierWave::Vips
+  include CarrierWave::MiniMagick
+  # include CarrierWave::Vips
 
   # Choose what kind of storage to use for this uploader:
   storage :file
