c10bca5464aa902c5d46c91479cf9a2c198b17d8
dirceupj <dirceuu@gmail.com>
2018-11-11 00:28:42 -0200

    add photos model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_021129) do
+ActiveRecord::Schema.define(version: 2018_11_11_022619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2018_11_11_021129) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "station_id"
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["station_id"], name: "index_photos_on_station_id"
   end
 
   create_table "stations", force: :cascade do |t|
