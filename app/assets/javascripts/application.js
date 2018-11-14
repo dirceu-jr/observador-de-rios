71f4ae650901d5e2acf3e76f37f5f9b0057c94b6
dirceupj <dirceuu@gmail.com>
2018-11-14 11:36:07 -0200

    refactor station page js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,4 +26,264 @@
 //= require leaflet-dvf.js
 //= require moment.min.js
 
-//= require home.js
+//= require stations.js
+
+var
+    breakCards = true,
+    
+    searchVisible = 0,
+    transparent = true,
+    
+    transparentDemo = true,
+    fixedTop = false,
+    
+    mobile_menu_visible = 0,
+    mobile_menu_initialized = false,
+    toggle_initialized = false,
+    bootstrap_nav_initialized = false
+;
+
+(function () {
+    var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+    if (isWindows) {
+        // if we are on windows OS we activate the perfectScrollbar function
+        $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+
+        $('html').addClass('perfect-scrollbar-on');
+    } else {
+        $('html').addClass('perfect-scrollbar-off');
+    }
+})();
+
+$(document).on('turbolinks:load', function () {
+
+    $('body').bootstrapMaterialDesign();
+
+    $sidebar = $('.sidebar');
+
+    app.initSidebarsCheck();
+
+    window_width = $(window).width();
+
+    // check if there is an image set for the sidebar's background
+    app.checkSidebarImage();
+
+    //  Activate the tooltips
+    $('[rel="tooltip"]').tooltip();
+
+    $('.form-control').on("focus", function () {
+        $(this).parent('.input-group').addClass("input-group-focus");
+    }).on("blur", function () {
+        $(this).parent(".input-group").removeClass("input-group-focus");
+    });
+
+    // remove class has-error for checkbox validation
+    $('input[type="checkbox"][required="true"], input[type="radio"][required="true"]').on('click', function () {
+        if ($(this).hasClass('error')) {
+            $(this).closest('div').removeClass('has-error');
+        }
+    });
+
+});
+
+// activate collapse right menu when the windows is resized
+$(window).resize(function () {
+    app.initSidebarsCheck();
+});
+
+$(document).on('click', '.navbar-toggler', function () {
+    var $toggle = $(this);
+
+    if (mobile_menu_visible == 1) {
+        $('html').removeClass('nav-open');
+
+        $('.close-layer').remove();
+        setTimeout(function () {
+            $toggle.removeClass('toggled');
+        }, 400);
+
+        mobile_menu_visible = 0;
+    } else {
+        setTimeout(function () {
+            $toggle.addClass('toggled');
+        }, 430);
+
+        var $layer = $('<div class="close-layer"></div>');
+
+        if ($('body').find('.main-panel').length != 0) {
+            $layer.appendTo(".main-panel");
+
+        } else if (($('body').hasClass('off-canvas-sidebar'))) {
+            $layer.appendTo(".wrapper-full-page");
+        }
+
+        setTimeout(function () {
+            $layer.addClass('visible');
+        }, 100);
+
+        $layer.click(function () {
+            $('html').removeClass('nav-open');
+            mobile_menu_visible = 0;
+
+            $layer.removeClass('visible');
+
+            setTimeout(function () {
+                $layer.remove();
+                $toggle.removeClass('toggled');
+
+            }, 400);
+        });
+
+        $('html').addClass('nav-open');
+        mobile_menu_visible = 1;
+
+    }
+
+});
+
+var app = {
+    misc: {
+        navbar_menu_visible: 0,
+        active_collapse: true,
+        disabled_collapse_init: 0
+    },
+
+    checkSidebarImage: function () {
+        $sidebar = $('.sidebar');
+        image_src = $sidebar.data('image');
+
+        if (image_src !== undefined) {
+            sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>';
+            $sidebar.append(sidebar_container);
+        }
+    },
+
+    showNotification: function (from, align) {
+        type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
+
+        color = Math.floor((Math.random() * 6) + 1);
+
+        $.notify({
+            icon: "add_alert",
+            message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+        }, {
+            type: type[color],
+            timer: 3000,
+            placement: {
+                from: from,
+                align: align
+            }
+        });
+    },
+
+    initSidebarsCheck: function () {
+        if ($(window).width() <= 991) {
+            if ($sidebar.length != 0) {
+                app.initRightMenu();
+            }
+        }
+    },
+
+    initMinimizeSidebar: function () {
+
+        $('#minimizeSidebar').click(function () {
+            var $btn = $(this);
+
+            if (app.misc.sidebar_mini_active == true) {
+                $('body').removeClass('sidebar-mini');
+                app.misc.sidebar_mini_active = false;
+            } else {
+                $('body').addClass('sidebar-mini');
+                app.misc.sidebar_mini_active = true;
+            }
+
+            // we simulate the window Resize so the charts will get updated in realtime.
+            var simulateWindowResize = setInterval(function () {
+                window.dispatchEvent(new Event('resize'));
+            }, 180);
+
+            // we stop the simulation of Window Resize after the animations are completed
+            setTimeout(function () {
+                clearInterval(simulateWindowResize);
+            }, 1000);
+        });
+    },
+
+    checkScrollForTransparentNavbar: debounce(function () {
+        if ($(document).scrollTop() > 260) {
+            if (transparent) {
+                transparent = false;
+                $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+            }
+        } else {
+            if (!transparent) {
+                transparent = true;
+                $('.navbar-color-on-scroll').addClass('navbar-transparent');
+            }
+        }
+    }, 17),
+
+
+    initRightMenu: debounce(function () {
+        $sidebar_wrapper = $('.sidebar-wrapper');
+
+        if (!mobile_menu_initialized) {
+            $navbar = $('nav').find('.navbar-collapse').children('.navbar-nav');
+
+            mobile_menu_content = '';
+
+            nav_content = $navbar.html();
+            nav_content = '<ul class="nav navbar-nav nav-mobile-menu">' + nav_content + '</ul>';
+
+            navbar_form = $('nav').find('.navbar-form').get(0).outerHTML;
+
+            $sidebar_nav = $sidebar_wrapper.find(' > .nav');
+
+            // insert the navbar form before the sidebar list
+            $nav_content = $(nav_content);
+            $navbar_form = $(navbar_form);
+            $nav_content.insertBefore($sidebar_nav);
+            $navbar_form.insertBefore($nav_content);
+
+            $(".sidebar-wrapper .dropdown .dropdown-menu > li > a").click(function (event) {
+                event.stopPropagation();
+
+            });
+
+            // simulate resize so all the charts/maps will be redrawn
+            window.dispatchEvent(new Event('resize'));
+
+            mobile_menu_initialized = true;
+        } else {
+            if ($(window).width() > 991) {
+                // reset all the additions that we made for the sidebar wrapper only if the screen is bigger than 991px
+                $sidebar_wrapper.find('.navbar-form').remove();
+                $sidebar_wrapper.find('.nav-mobile-menu').remove();
+
+                mobile_menu_initialized = false;
+            }
+        }
+    }, 200)
+
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    }
+}
