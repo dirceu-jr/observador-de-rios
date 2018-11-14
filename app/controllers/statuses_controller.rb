c23955ef5cbf1b0c6f48c12118d43c3ac1adaea0
dirceupj <dirceuu@gmail.com>
2018-11-14 14:52:08 -0200

    creates api in order to recieve statuses update from probe

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,20 @@
+class StatusesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    # "latitude": 5, "longitude":5, "ph":5, "orp":5, "od":5, "temperature":5, "co2":5, "conductivity":5}}
+    # curl -d '{"status":{"station_id":"1","latitude":"5","longitude":"5","ph":"5","orp":"5","od":"5","temperature":"5","co2":"5","conductivity":"5"}}' -H "Content-Type: application/json" -X POST http://localhost:3000/statuses
+    def create
+        @status = Status.new(status_params)
+        p @status
+        if @status.save
+            render json: @status
+        else
+            p @status
+            render json: {error: "invalid request"}
+        end
+    end
+
+    private
+    def status_params
+      params.require(:status).permit(:station_id, :latitude, :longitude, :ph, :orp, :od, :temperature, :co2, :conductivity)
+    end
+end
